62fc1cafc11a47220844028b97d69cf0bb024dd3
JeremiasOviedo <JeremiasOviedo.Dev@gmail.com>
2022-11-01 18:26:17 -0300

    added small fixes

--- src/main/java/com/alkemy/wallet/model/User.java
+++ src/main/java/com/alkemy/wallet/model/User.java
@@ -1,2 +1,55 @@
-package com.alkemy.wallet.model;public class User {
+package com.alkemy.wallet.model;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
+
+@Entity
+@Table(name = "users")
+
+@SQLDelete(sql = "UPDATE user SET deleted = true WHERE id=?")
+@Where(clause = "deleted=false")
+@Getter
+@Setter
+public class User implements Serializable {
+
+
+  public static final long serialVersionUID = 1L;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long idUser;
+
+  @Column(name = "FIRST_NAME", nullable = false)
+  private String firstName;
+
+  @Column(name = "LAST_NAME", nullable = false)
+  private String lastName;
+
+  @Column(name = "EMAIL", nullable = false)
+  private String email;
+
+  @Column(name = "PASSWORD", nullable = false)
+  private String password;
+
+  @ManyToOne()
+  @JoinColumn(name = "roleId")
+  private Role role;
+
+  @OneToMany(mappedBy = "user",
+      fetch = FetchType.EAGER,
+      cascade = {
+          CascadeType.DETACH,
+          CascadeType.MERGE,
+          CascadeType.REFRESH,
+          CascadeType.PERSIST
+      })
+
+  private List<Account> accounts;
+
+  private boolean delete = Boolean.FALSE;
+
 }
