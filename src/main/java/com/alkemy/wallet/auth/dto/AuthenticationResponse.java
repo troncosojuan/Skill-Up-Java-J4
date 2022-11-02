979c72d68fc5143f21e216b586bd058e7b70bb73
Troncoso Juan <troncosojuanbernabe@gmail.com>
2022-11-01 23:13:30 -0300

    Se realiza la authentification y su endpoint, por otra parte se añade la logica de JWT y devolver el token al usuario

--- src/main/java/com/alkemy/wallet/auth/dto/AuthenticationResponse.java
+++ src/main/java/com/alkemy/wallet/auth/dto/AuthenticationResponse.java
@@ -1,5 +1,12 @@
 package com.alkemy.wallet.auth.dto;
 
-public class AuthenticationResponse {
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+@AllArgsConstructor
+public class AuthenticationResponse {
+  private String jwt;
 }
--- src/main/java/com/alkemy/wallet/auth/service/UserDetailsCustomService.java
+++ src/main/java/com/alkemy/wallet/auth/service/UserDetailsCustomService.java
@@ -1,5 +1,45 @@
 package com.alkemy.wallet.auth.service;
 
-public class UserDetailsCustomService {
+import com.alkemy.wallet.auth.dto.UserAuthDto;
+import com.alkemy.wallet.auth.repository.UserRepository;
+import com.alkemy.wallet.exception.RepeatedUsername;
+import java.util.Collections;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsCustomService implements UserDetailsService {
+
+  @Autowired
+  private UserRepository userRepository;
+
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    UserEntity userEntity = userRepository.findByUsername(username);
+    if (userEntity == null) {
+      throw new UsernameNotFoundException("username or password not found");
+    }
+    return new User(userEntity.getUsername(), userEntity.getPassword(), Collections.emptyList());
+  }
+
+  public void save(@Valid UserAuthDto userDto) throws RepeatedUsername {
+    BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
+
+    if (userRepository.findByUsername(userDto.getUsername()) != null) {
+      throw new RepeatedUsername("Username repetido");
+    }
+    UserEntity userEntity = new UserEntity();
+    userEntity.setUsername(userDto.getUsername());
+    userEntity.setPassword(bCryptPasswordEncoder.encode(userDto.getPassword()));
+
+  }
+
 
 }
