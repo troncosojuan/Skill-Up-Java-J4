bb23075112c52f633d561c9b4989842f17d6c08e
aesaries <91644783+aesaries@users.noreply.github.com>
2022-11-02 23:08:05 -0300

    added new entity, Role

--- src/main/java/com/alkemy/wallet/entity/RoleEntity.java
+++ src/main/java/com/alkemy/wallet/entity/RoleEntity.java
@@ -1,5 +1,36 @@
 package com.alkemy.wallet.entity;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+@Getter
+@Setter
 public class RoleEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(nullable = false, length = 50, unique = true)
+    private String name;
+
+    public RoleEntity() { }
+
+    public RoleEntity(String name) {
+        this.name = name;
+    }
+
+    public RoleEntity(Integer id) {
+        this.id = id;
+    }
+
+
+    @Override
+    public String toString() {
+        return this.name;
+    }
 
 }
